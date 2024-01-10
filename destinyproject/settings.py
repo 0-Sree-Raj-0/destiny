ce7b909dd5d81e91c7e7adcb35c69c409fb48c28
0-Sree-Raj-0 <150782014+0-Sree-Raj-0@users.noreply.github.com>
2024-01-10 12:49:25 +0530

    Update settings.py

--- destinyproject/settings.py
+++ destinyproject/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = 'destiny/destinyapp/static/'
 STATICFILES_DIRS=[
    os.path.join(BASE_DIR,'destiny/destinyapp/static' ),
 ]
